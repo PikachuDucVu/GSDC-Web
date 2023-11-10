73eb8a338b37e28a1c808536a4e5a903fa64441e
PikachuDucvu <ducvubn1@gmail.com>
2023-11-10 21:21:23 +0700

    add input text verify are you a robot?

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
         <div>login</div>
         <input type="text" placeholder="username" />
         <input type="text" placeholder="password" />
+        <input type="text" placeholder="verify you not a robot" />
       </div>
     </>
   );
@@ -15,6 +16,15 @@ function App() {
 //Git
 
 // git init: Start git trong repo
-// git add: Thêm code đã sửa hoặc tạo mới vào trong stagging
+// git add: Thêm code đã sửa hoặc tạo mới vào trong stagging // git add .
+// git commit: Đưa ra message về code đã sửa gì, ... // git commit -m 'message'
+// git remote: Remote tới repo mà chúng ta đang làm việc //git remote add origin 'URL'
+// git branch: Tạo nhánh làm việc // git branch -M main
+// git push: đẩy code lên github // git push -u origin main // git push
+
+// update code
+// git add .
+// git commit -m 'message'
+// git push
 
 export default App;

cd5c01844f3e76f695b0499bccfade081f4663cd
PikachuDucvu <ducvubn1@gmail.com>
2023-11-10 21:11:12 +0700

    the first version code

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col w-full h-full border">
+        <div>login</div>
+        <input type="text" placeholder="username" />
+        <input type="text" placeholder="password" />
+      </div>
+    </>
+  );
+}
+
+//Git
+
+// git init: Start git trong repo
+// git add: Thêm code đã sửa hoặc tạo mới vào trong stagging
+
+export default App;
